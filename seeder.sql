73bbdfd441154367f7e91b520c6089fd42c5ea45
unknown <dedoa82.as@gmail.com>
2022-05-11 13:45:09 +0200

    DB : creation , insertion

---
+++ seeder.sql
@@ -0,0 +1,36 @@
+INSERT INTO
+    cars
+VALUES
+    (uuid(), 'Civic', 'Honda', 250000),
+    (uuid(), 'City', 'Honda', 150000),
+    (uuid(), 'Lancer', 'Mitsubishi', 130000),
+    (uuid(), 'Shark', 'Mitsubishi', 170000),
+    (uuid(), 'Corolla', 'Toyota', 200000),
+    (uuid(), 'C200', 'Mercedes', 500000),
+
+
+INSERT INTO
+    option_cars
+VALUES
+    (uuid(), 'Smart key', 1000, '44556677'),
+    (uuid(), 'Parking sensor', 750, '22113344'),
+    (uuid(), 'fog', 300, '1223344'),
+    (uuid(), 'Smart key', 4000, '77886655'),
+    (uuid(), 'Parking sensor', 2500, '77886655')
+
+
+
+INSERT INTO `sales_persons` (`id`, `name`, `phone`) VALUES ('AH8877', 'Sayed Hussein', '0111234567'), ('VH7766', 'Nader Galal', NULL), ('MN1212', 'Samir Ayob', '0100556699');
+
+
+INSERT INTO `sales` VALUES 
+    (uuid(), 'AH8877', '1223344', 250750, '2015-03-14'),
+    (uuid(), 'AH8877', '11223345', 250750, '2015-10-25'),
+    (uuid(), 'VH7766', '44556677', 250750, '2015-03-30'),
+    (uuid(), 'MN1212', '1223344', 250750, '2015-11-02'),
+    (uuid(), 'VH7766', '44556678', 250750, '2015-01-07'),
+    (uuid(), 'AH8877', '77886655', 250750, '2015-03-09'),
+    (uuid(), 'MN1212', '77886655', 250750, '2015-03-14')
+
+
+
